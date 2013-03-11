65d51312b5724eec38b87ccf56b50832873393f4
Johnny Broadway <johnny@johnnybroadway.com>
2013-03-11 14:53:04 -0500

    Added user/login to account and search

--- handlers/account.php
+++ handlers/account.php
@@ -2,6 +2,12 @@
 
 namespace saasy;
 
+if (! \User::require_login ()) {
+	$page->title = __ ('Members');
+	echo $this->run ('user/login');
+	return;
+}
+
 $page->title = __ ('Account');
 
 echo '<p>...</p>';
