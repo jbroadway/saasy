d137027857db39468d2ebb7785f050fa5ce19877
Johnny Broadway <johnny@johnnybroadway.com>
2013-03-12 14:12:25 -0500

    Added outline of account owner form

--- handlers/account.php
+++ handlers/account.php
@@ -14,10 +14,25 @@ if (! $res) {
 
 $page->title = __ ('Account');
 
-echo $tpl->render (
-	'saasy/account',
-	array (
-	)
+$form = new \Form ('post', $this);
+
+$org = App::org ();
+
+$form->data = array (
+	'name' => \User::val ('name'),
+	'email' => \User::val ('email'),
+	'org_name' => $org->name,
+	'subdomain' => $org->subdomain
 );
+
+$acct = App::acct ();
+if ($acct->type === 'owner') {
+	$form->view = 'saasy/account_owner';
+	$page->add_script ('/apps/saasy/js/bootstrap-filestyle-0.1.0.min.js');
+}
+
+echo $form->handle (function ($form) use ($page, $org, $acct) {
+	info ($_POST);
+});
 
 ?>
